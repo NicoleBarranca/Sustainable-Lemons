ee904d5f1f75911d43fbdfc66b1a39ed1cf2804f
Nicole Barranca <nikkibarranca@gmail.com>
2022-04-03 16:17:10 -0400

    added DELETE routes to Category adn Tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -74,7 +74,22 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No Category found with this id" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(500).json(err);
+    });
 });
 
 module.exports = router;
